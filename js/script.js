4b9b715c70a4bf937f437fd2a6b08bd7f6cb2b22
sacarr98 <>
2023-10-12 07:22:49 +0000

    create an array in js to hold quiz questions and answers

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Questions that will be asked
+const Questions = [{
+    q: "What is the national language of Canada?",
+    a: [{ text: "English", isCorrect: false },
+    { text: "French", isCorrect: false },
+    { text: "Dutch", isCorrect: true },
+    { text: "Spanish", isCorrect: false }
+    ]
+
+},
+{
+    q: "Brazil is the biggest producer of?",
+    a: [{ text: "Rice", isCorrect: false, isSelected: false },
+    { text: "Potatoes", isCorrect: false },
+    { text: "Oil", isCorrect: false },
+    { text: "Coffee", isCorrect: true }
+    ]
+
+},
+{
+    q: "How many colors in the Rainbow?",
+    a: [{ text: "Five", isCorrect: false },
+    { text: "Six", isCorrect: false },
+    { text: "Seven", isCorrect: true },
+    { text: "Eight", isCorrect: false }
+    ]
+
+}
+
+];
