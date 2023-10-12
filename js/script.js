9e4bfa9f3edfae002295317ebee0e1d69272c970
sacarr98 <>
2023-10-12 07:28:58 +0000

    create a for loop within loadQues function to iterate through the questions and answers array and display in the questions panel

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Questions that will be asked
+// Questions that will be loaded into the question panel area
 const Questions = [{
     q: "What is the national language of Canada?",
     a: [{ text: "English", isCorrect: false },
@@ -32,11 +32,33 @@ const Questions = [{
 let currQuestion = 0;
 let score = 0;
 
+
+/**
+ *  The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ * loads questions from the array into the question panel area
+ */
 function loadQues() {
     const question = document.getElementById("ques");
     const opt = document.getElementById("opt");
 
     question.textContent = Questions[currQuestion].q;
     opt.innerHTML = "";
+
+    for (let i = 0; i < Questions[currQuestion].a.length; i++) {
+        const choicesdiv = document.createElement("div");
+        const choice = document.createElement("input");
+        const choiceLabel = document.createElement("label");
+
+        choice.type = "radio";
+        choice.name = "answer";
+        choice.value = i;
+
+        choiceLabel.textContent = Questions[currQuestion].a[i].text;
+
+        choicesdiv.appendChild(choice);
+        choicesdiv.appendChild(choiceLabel);
+        opt.appendChild(choicesdiv);
+    }
 
 }
