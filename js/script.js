ce72448badd9d7179aeaa3382ab214e65ec42345
sacarr98 <>
2023-10-13 07:15:17 +0000

    edit media queries for small screens

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Questions that will be loaded into the question panel area
 const Questions = [{
-    q: "What is the national language of Canada?",
-    a: [{ text: "English", isCorrect: false },
+    Qs: "What is the national language of Canada?",
+    As: [{ text: "English", isCorrect: false },
     { text: "French", isCorrect: false },
     { text: "Dutch", isCorrect: true },
     { text: "Spanish", isCorrect: false }
@@ -9,8 +9,8 @@ const Questions = [{
 
 },
 {
-    q: "Brazil is the biggest producer of?",
-    a: [{ text: "Rice", isCorrect: false, isSelected: false },
+    Qs: "Brazil is the biggest producer of?",
+    As: [{ text: "Rice", isCorrect: false, isSelected: false },
     { text: "Potatoes", isCorrect: false },
     { text: "Oil", isCorrect: false },
     { text: "Coffee", isCorrect: true }
@@ -18,8 +18,8 @@ const Questions = [{
 
 },
 {
-    q: "How many colors in the Rainbow?",
-    a: [{ text: "Five", isCorrect: false },
+    Qs: "How many colors in the Rainbow?",
+    As: [{ text: "Five", isCorrect: false },
     { text: "Six", isCorrect: false },
     { text: "Seven", isCorrect: true },
     { text: "Eight", isCorrect: false }
@@ -27,8 +27,8 @@ const Questions = [{
 
 },
 {
-    q: "Which religion has a God specified for each Rain, Money, Children, and Love?",
-    a: [{ text: "Islam", isCorrect: false },
+    Qs: "Which religion has a God specified for each Rain, Money, Children, and Love?",
+    As: [{ text: "Islam", isCorrect: false },
     { text: "Buddism", isCorrect: false },
     { text: "Hinduism", isCorrect: true },
     { text: "Jainism", isCorrect: false }
@@ -36,8 +36,8 @@ const Questions = [{
 
 },
 {
-    q: "What is the currency of Pakistan?",
-    a: [{ text: "Pound", isCorrect: false },
+    Qs: "What is the currency of Pakistan?",
+    As: [{ text: "Pound", isCorrect: false },
     { text: "Euro", isCorrect: false },
     { text: "Riyal", isCorrect: false },
     { text: "Rupee", isCorrect: true }
@@ -45,8 +45,8 @@ const Questions = [{
 
 },
 {
-    q: "Which popular Disney show was Miley Cyrus part of?",
-    a: [{ text: "Hannah Montanna", isCorrect: true },
+    Qs: "Which popular Disney show was Miley Cyrus part of?",
+    As: [{ text: "Hannah Montanna", isCorrect: true },
     { text: "Two Broke Girls", isCorrect: false },
     { text: "Live it like Beckham", isCorrect: false },
     { text: "Suite life of Jack and Cody", isCorrect: false }
@@ -68,7 +68,7 @@ function loadQues() {
     const question = document.getElementById("ques");
     const opt = document.getElementById("opt");
 
-    question.textContent = Questions[currQuestion].q;
+    question.textContent = Questions[currQuestion].Qs;
     opt.innerHTML = "";
 
     for (let i = 0; i < Questions[currQuestion].a.length; i++) {
@@ -80,7 +80,7 @@ function loadQues() {
         choice.name = "answer";
         choice.value = i;
 
-        choiceLabel.textContent = Questions[currQuestion].a[i].text;
+        choiceLabel.textContent = Questions[currQuestion].As[i].text;
 
         choicesdiv.appendChild(choice);
         choicesdiv.appendChild(choiceLabel);
@@ -140,7 +140,7 @@ function nextQuestion() {
  */
 function checkAns() {
     const selectedAns = parseInt(document.querySelector('input[name="answer"]:checked').value);
-    if (Questions[currQuestion].a[selectedAns].isCorrect) {
+    if (Questions[currQuestion].As[selectedAns].isCorrect) {
         alert("Hey! You got it right!");
         incrementScore();
         nextQuestion();
